10b4c58b9e271dd59bc7666cb76eef4397105b0b
LuizCaster21 <luizfernandovc21@gmail.com>
2025-02-19 12:12:16 -0300

    implementando a verificação de xeque

--- Chess-CSharp-Console/Tela.cs
+++ Chess-CSharp-Console/Tela.cs
@@ -14,6 +14,10 @@ namespace Chess_CSharp_Console
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if(partida.xeque)
+            {
+                Console.WriteLine("XEQUE!");
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
