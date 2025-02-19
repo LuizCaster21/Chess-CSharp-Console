7e5bcce248cb3bc5c622a688eaf51a63b56bc54b
LuizCaster21 <luizfernandovc21@gmail.com>
2025-02-19 08:30:10 -0300

    criando classe posicaoXadrez

--- Chess-CSharp-Console/Program.cs
+++ Chess-CSharp-Console/Program.cs
@@ -9,21 +9,10 @@ namespace ChessConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 8));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 4));
-
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.toPosicao());
         }
     }
 }
