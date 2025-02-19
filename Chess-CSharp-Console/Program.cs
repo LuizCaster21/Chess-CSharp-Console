90c140270c2c0f62e1e7f604ea0524f7674a5770
LuizCaster21 <luizfernandovc21@gmail.com>
2025-02-19 10:24:29 -0300

    sistema de turnos

--- Chess-CSharp-Console/Program.cs
+++ Chess-CSharp-Console/Program.cs
@@ -16,6 +16,9 @@ namespace ChessConsole
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
@@ -25,10 +28,10 @@ namespace ChessConsole
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    Console.Write("Destino:");
+                    Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
                 }
             }
             catch(TabuleiroException e)
